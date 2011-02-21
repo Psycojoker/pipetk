1c41a85ae25f4cb207c0ea2313b8651495ea2487
Laurent Peuch <cortex@worlddomination.be>
2011-02-21 05:20:29 +0100

    first commit, only a setup.py for the moment since this is intented to regroup bunch of script for distribution purpose

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='PipeTK',
+      version='0.1',
+      description='Collection of small scripts that modify input received via shell pipe',
+      author='Laurent Peuch',
+      author_email='cortex@worlddomination.be',
+      url='http://blog.worlddomination.be/pipetk',
+      install_requires=[],
+      packages=[],
+      scripts=[],
+      license = "GPLv3+",
+      keywords="sh bash shell pipe stream",
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
