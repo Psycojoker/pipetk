9ea101653014ab4a72f6dd85e86246991cfe2e46
Laurent Peuch <cortex@worlddomination.be>
2011-10-20 19:28:14 +0200

    [fix] oups, forget to rename README in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='PipeTK',
       version='0.1.2',
       description='Collection of small scripts that modify input received via shell pipe',
-      long_description = open('README').read(),
+      long_description = open('README.rst').read(),
       author='Laurent Peuch',
       author_email='cortex@worlddomination.be',
       url='http://blog.worlddomination.be/pipetk',
