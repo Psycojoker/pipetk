c88e8a122a24b04f120004b6373a2d8a517c4b69
Laurent Peuch <cortex@worlddomination.be>
2012-02-09 07:42:57 +0100

    [mod] swap symlink and README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='PipeTK',
       version='0.2',
       description='Collection of small scripts that modify input received via shell pipe',
-      long_description=open('README.rst').read(),
+      long_description=open('README').read(),
       author='Laurent Peuch',
       author_email='cortex@worlddomination.be',
       url='http://worlddomination.be/projects/pipetk.html',
